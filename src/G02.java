9816b99e0e648aa73258453f739126e89079ab8e
ercnozkn <ozknercn.54@gmail.com>
2023-04-12 11:29:16 +0200

    sonTekrar

---
+++ src/G02.java
@@ -0,0 +1,24 @@
+public class G02 {
+
+    /*
+    proje uzerinde
+    calisirken temellde uc adimda calisiyoruz
+
+    1- projelerimizi git ile iliskkilendir
+    git init
+    2- projemizde degisiklik yapti isek versiyon olustururuz
+    git add .
+    git commit -m"versiyon adi"
+    3- github a yollama
+    ilk olarak github da repo olustur
+    git remote add origin repouradresi (githubdan kopyalanir)
+
+    var oln repo adresi degisecekse
+    git remote set-url (yeni adres)
+
+    git status ile yapilan degisiklikler gorulur
+
+
+    4- push edilir
+     */
+}
